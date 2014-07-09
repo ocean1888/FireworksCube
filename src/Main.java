e47d91e53c12535b0be3bde371dbb56ea8d64534
unknown <bic08002@byui.edu>
2014-07-09 15:01:58 -0600

    Heres jsut a test this does no work all the way balh blah ablah alsjh

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,7 +22,7 @@ import static org.lwjgl.util.glu.GLU.gluPerspective;
 public class Main {
 private boolean done = false;
     private boolean fullscreen = false;
-    private final String windowTitle = "NeHe's OpenGL Lesson 5 for LWJGL (3D Shapes)";
+    private final String windowTitle = "Fireworks Cube";
     private boolean f1 = false;
     private float tranZ = -6;   // negitive is further into the screen
     private float rotY = 1;     // positive is pressing to the rigth
@@ -28,7 +30,10 @@ private boolean done = false;
     private float rquad;                // Angle For The Quad     ( NEW )
     private Boolean zoomed;
     private DisplayMode displayMode;
-
+    private FireworkShow fShow;
+    private List<Spark> sparks;
+    //determins if there is a firework
+    private boolean fg = false;
     public static void main(String args[]) {
         boolean fullscreen = false;
         if(args.length>0) {
@@ -73,6 +78,20 @@ private boolean done = false;
         }
         if(!Keyboard.isKeyDown(Keyboard.KEY_F1)) {          // Is F1 Being Pressed?
             f1 = false;
+        }
+        if(Mouse.isButtonDown(0))
+        {
+            System.out.println("x = "+(float)((float)Mouse.getX()/(float)displayMode.getWidth()));
+            System.out.println("Y = "+Mouse.getY());
+            sparks = Firework.createFirework();
+            fg = true;
+            for(int i = 0; i < 300; i++)
+            {
+                render();
+                Display.update();
+            }
+            fg = false;
+//            explode();
         }
         if(Keyboard.isKeyDown(Keyboard.KEY_SPACE))
         {
@@ -196,22 +215,51 @@ private boolean done = false;
     private boolean render() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);        
        
-        glLoadIdentity();                          // Reset The Current Modelview Matrix
-        glTranslatef(0.0f,0.0f,(float)tranZ);             // Move/translate the cube back
-        glRotatef(rquad, (float)rotX,(float)rotY,0.0f);               // Rotate The Quad On The Y axis ( NEW )
-        glColor3f(0.5f,0.5f,1.0f);                 // Set The Color To Blue One Time Only
-        glBegin(GL_LINES);                        // Draw A Quad
-            glColor3f(0.45f,.85f,1.0f);             // Set The Color To Green
-            /*glVertex3f( 1.0f, 1.0f,-1.0f);         // Top Right Of The Quad (Top)
-            glVertex3f(-1.0f, 1.0f,-1.0f);         // Top Left Of The Quad (Top)
-            glVertex3f(-1.0f, 1.0f, 1.0f);         // Bottom Left Of The Quad (Top)
-            glVertex3f( 1.0f, 1.0f, 1.0f);*/         // Bottom Right Of The Quad (Top)
-            /*glColor3f(1.0f,0.5f,0.0f);             // Set The Color To Orange
-            glVertex3f( 1.0f,-1.0f, 1.0f);         // Top Right Of The Quad (Bottom)
-            glVertex3f(-1.0f,-1.0f, 1.0f);         // Top Left Of The Quad (Bottom)
-            glVertex3f(-1.0f,-1.0f,-1.0f);         // Bottom Left Of The Quad (Bottom)
-            glVertex3f( 1.0f,-1.0f,-1.0f);*/         // Bottom Right Of The Quad (Bottom)
-            //glColor3f(1.0f,0.0f,0.0f);             // Set The Color To Red
+        glLoadIdentity(); // Reset The Current Modelview Matrix
+        //where we draw the firework and lighting
+        glTranslatef(0.0f,0.0f,(float)tranZ); // Move/translate the cube back
+        
+        glRotatef(rquad, (float)rotX,(float)rotY,0.0f); // Rotate The Quad On The Y axis ( NEW )
+        drawOutline();
+        //apply the lighting
+        // if we come up with a way to randomize these numbers and the 
+        // number of sets then we can create the "random" landscape
+        float x =  0.0f;
+        float y = -1.0f;
+        float z =  0.0f;
+        float w =  0.5f;
+        float h =  1.0f;
+        float d =  0.5f;
+        squar(x, y, z, w, h, d);
+        x = -0.2f;
+        z = 0.8f;
+        w = .2f;
+        h = .6f;
+        d = .2f;
+        squar(x, y, z, w, h, d);
+        return true;
+    }
+    
+    /*
+    * in the futur perhaps we can leverage this
+    */
+    /*
+    private boolean light()
+    {
+        GLfloat light_ambient[] = {0.0, 0.0, 0.0, 1.0};
+        glLight(1, GL_2D, null);
+        glLi
+        return true;
+    }*/
+    
+    
+    private boolean drawOutline()
+    {
+        glPushMatrix();
+        {
+        glBegin(GL_LINES); 
+        {
+            glColor3f(0.45f,.85f,1.0f);                     
             glVertex3f( 1.0f, 1.0f, 1.0f);         // Top Right Of The Quad (Front)
             glVertex3f(-1.0f, 1.0f, 1.0f);         // Top Left Of The Quad (Front)
             glVertex3f(-1.0f,-1.0f, 1.0f);         // Bottom Left Of The Quad (Front)
@@ -222,8 +270,7 @@ private boolean done = false;
             //right back line
             glVertex3f(1.0f, 1.0f, 1);
             glVertex3f(1.0f, -1.0f, 1);
-            //top back line
-            //glColor3f(1.0f,1.0f,0.0f);             // Set The Color To Yellow
+            //top back line         
             glVertex3f( 1.0f,-1.0f,-1.0f);         // Bottom Left Of The Quad (Back)
             glVertex3f(-1.0f,-1.0f,-1.0f);         // Bottom Right Of The Quad (Back)
             //bottom back line
@@ -245,7 +292,58 @@ private boolean done = false;
             glVertex3f( 1.0f, 1.0f, 1.0f);         // Top Left Of The Quad (Right)
             glVertex3f( 1.0f,-1.0f, 1.0f);         // Bottom Left Of The Quad (Right)
             glVertex3f( 1.0f,-1.0f,-1.0f);         // Bottom Right Of The Quad (Right)
+        }
         glEnd();                                       // Done Drawing The Quad
+        }
+        glPopMatrix();
+        return true;
+    }
+    
+    /*
+     * please insure that the position is set at the bottom left
+    */
+    private boolean squar(float x, float y, float z, 
+                          float w, float h, float d)
+    {        
+        glPushMatrix();
+        {
+            glBegin(GL_QUADS);
+            {
+                glColor3f(0.2f, 0.2f, 0.2f);
+                //front
+                glVertex3f(x, y, z);
+                glVertex3f(x + w, y, z);
+                glVertex3f(x + w, y + h, z);
+                glVertex3f(x, y + h, z);
+                //left side
+                glVertex3f(x, y, z);
+                glVertex3f(x, y, z + d);
+                glVertex3f(x, y + h, z + d);
+                glVertex3f(x, y, z);
+                //back side
+                glVertex3f(x, y, z + d);
+                glVertex3f(x + w, y, z + d);
+                glVertex3f(x + w, y + h, z + d);
+                glVertex3f(x, y + h, z + d);
+                //rigt side
+                glVertex3f(x + w, y, z);
+                glVertex3f(x + w, y, z + d);
+                glVertex3f(x + w, y + h, z + d);
+                glVertex3f(x + w, y + h, z);
+                //top side
+                glVertex3f(x, y + h, z);
+                glVertex3f(x + w, y + h, z);
+                glVertex3f(x + w, y + h, z + d);
+                glVertex3f(x, y + h, z + d);
+                //bottom
+                glVertex3f(x, y, z);
+                glVertex3f(x + w, y, z);
+                glVertex3f(x + w, y, z + d);
+                glVertex3f(x, y, z + d);              
+            }
+            glEnd();
+        }
+        glPopMatrix();
         return true;
     }
     private void createWindow() throws Exception {
